719fcf245e181d5a5db955d96847d096533420b2
Angel Rego <angelrego@gmail.com>
2019-04-23 13:15:49 +0200

    mqtt: prepare for CRC generation

---
+++ SCOTTProducer/src/SCOTTProducer/logic/CRC.java
@@ -0,0 +1,14 @@
+package SCOTTProducer.logic;
+
+/**
+ * @see https://rcccalc.com
+ *
+ */
+public class CRC {
+	public static void main(String[] args) {
+		String data = "0123456789";
+		// As example, string “0123456789” will obtain 0xA684C7C6.
+//		int CRC = (int) System.currentTimeMillis() & data.getBytes();
+//		System.out.println(CRC);
+	}
+}
