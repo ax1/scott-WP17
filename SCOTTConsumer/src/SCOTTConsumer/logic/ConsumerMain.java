367855c2edcad133b97a717e0b4ebc37586f0b93
Angel Rego <angelrego@gmail.com>
2019-06-06 13:29:18 +0200

    consumer: new version using PresenceSubscription
    RAW subscription (no mqtt no canonical etc) but functional, this consumer can retrieve harvester ids from functional producers
    
    This code is by using the new version of SMOOL, the whole KP will be commited in the next commit. This atomic commit is to get only the Consumer code itself.

--- SCOTTConsumer/src/SCOTTConsumer/logic/ConsumerMain.java
+++ SCOTTConsumer/src/SCOTTConsumer/logic/ConsumerMain.java
@@ -1,70 +1,48 @@
 package SCOTTConsumer.logic;
 
 import java.io.IOException;
-import java.util.Observable;
 import java.util.Observer;
 
-import SCOTTConsumer.api.*;
-import SCOTTConsumer.model.smoolcore.impl.MessageReceiveSensor;
+import org.smool.kpi.model.exception.KPIModelException;
 
-/**
- * TODOs
- * 1-review observer & observer 2 (equivalents) and discard one
- * 2-check connect() methods and implement/adapt as library methods in SmoolKP (handier than existing connection methods)
- * 3-look at the exception management in new connect() methods and compare to exisiting boolean result in smoolkp
- * 4-add a infinite loop with sleep for re-triying connections when disconnected (i.e: every 10000 millis)
- *
- */
+import SCOTTConsumer.api.Consumer;
+import SCOTTConsumer.api.PresenceSensorSubscription;
+import SCOTTConsumer.api.SmoolKP;
+import SCOTTConsumer.model.smoolcore.impl.PresenceSensor;
+
 public class ConsumerMain {
-	
-	public static void main(String [] args) throws Exception {
-		final String name="SCOTTConsumer_"+Long.toString(System.nanoTime());
-		new ConsumerMain(name);
-	}
-	
-	public ConsumerMain(String name) throws Exception {
+
+	public ConsumerMain() throws Exception {
+		final String name = "SCOTTConsumer_" + Long.toString(System.nanoTime());
 		SmoolKP.setKPName(name);
-		//-------------connect to SIB---------------------------------------------
-		//connect();
-		connect("sib1", "smool.tecnalia.com","80");
-		//--------------delay to prevent subscription error---------
-		try {Thread.sleep(1000);}catch(Exception e) {;}
-		//--------------add custom observer to subscription-------
-		
-		Observer observer = (o, concept)->	System.out.println("message arrived: "+((MessageReceiveSensor)concept).getMessage().getBody());
-		
-		Observer observer2=new Observer() {
-			public void update(Observable o, Object concept) {
-				System.out.println("message arrived: "+((MessageReceiveSensor)concept).getMessage().getBody());
-				
-			}
+		// -------------connect to SIB---------------------------------------------
+		SmoolKP.connect("sib1", "smool.tecnalia.com", 80);
+
+		// --------------add custom observer to subscription-------
+		Observer observer = (o, concept) -> {
+			PresenceSensor sensor = (PresenceSensor) concept;
+			System.out.println(sensor.getDeviceID() + " ->  harvester " + sensor.getPresence().getDataID());
 		};
-		
+
 		Consumer consumer = SmoolKP.getConsumer();
-		consumer.subscribeToMessageReceiveSensor(new MessageReceiveSensorSubscription(observer2), null);
-		
-
+		consumer.subscribeToPresenceSensor(new PresenceSensorSubscription(observer), null);
+		Thread.sleep(Long.MAX_VALUE);
 	}
-	
-	private void connect() throws IOException {
-		SmoolKP.synchronousSearch(1000, true);
-		if (SmoolKP.isSIBfound()) {
-			System.out.println("Found a SIB: " + SmoolKP.getDiscoveredSIBs().get(0).getSIBName());
-			if (SmoolKP.connectToSIB(1000)) {
-				System.out.println("Successfully connected to SIB");
-			} else {
-				throw new IOException ("Unable to connect to SIB");
+
+	public static void main(String[] args) throws Exception {
+		// Logger.setDebugging(true);
+		// Logger.setDebugLevel(4);
+		while (true) {
+			try {
+				new ConsumerMain();
+			} catch (KPIModelException | IOException e) {
+				e.printStackTrace();
+				Thread.sleep(10000);
+				System.out.println("RECONNECTING");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(1);
 			}
-		}
-	}
-	
-
-	private void connect(String name, String ip, String port) throws IOException {
-		boolean connected = SmoolKP.connectToSIB(name,ip, port,0);
-		if (connected) {
-			System.out.println("Successfully connected to SIB");
-		} else {
-			throw new IOException ("Unable to connect to SIB");
 		}
 	}
 }
