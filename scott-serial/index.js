2d834fd69736e37b96c5969ef8feb694748c0096
Angel Rego <angelrego@gmail.com>
2019-06-10 10:56:38 +0200

    serial: minor changes

--- scott-serial/index.js
+++ scott-serial/index.js
@@ -1,8 +1,8 @@
-const { Socket } = require('net')
+const net = require('net')
 const PORT = 4444
 
 function socketClient(data) {
-  const s = Socket()
+  const s = net.Socket()
   s.connect(PORT)
   s.write(data)
   s.end()
@@ -10,7 +10,7 @@ function socketClient(data) {
 
 
 function ping() {
-  //ping is needed to keep socket running because if NAT in the middle, the default tcp_alive wont work beecause default is 2 hours and the NAT closes automatically connections idle every 5 minutes
+  //ping is needed to keep socket running because if NAT in the middle, the default tcp_alive wont work because default is 2 hours and the NAT closes automatically connections idle every 5 minutes
   socketClient('PING')
 }
 
