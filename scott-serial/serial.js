52c90c054ee7f268146d54aa7910e80e950311b2
Angel Rego <angelrego@gmail.com>
2019-07-02 10:43:54 +0200

    serial: detect close socket event and treat it as an error

--- scott-serial/serial.js
+++ scott-serial/serial.js
@@ -4,6 +4,7 @@ const Readline = require('@serialport/parser-readline')
 function init(cb) {
   const port = new SerialPort('/dev/ttyUSB0', { baudRate: 9600 })
   port.on('error', err => { console.error(err); process.exit(1) })
+  port.on('close', err => { console.error(err); process.exit(1) })  // if temporary disconnection, error event is not triggered, but close is, so treat close the same as error
   const parser = new Readline()
   port.pipe(parser)
   parser.on('data', line => cb(line.replace(" ", "").replace('\r', '')))
