5826071cc1192ee3157189144026eadf7ff1c09b
Angel Rego <angelrego@gmail.com>
2019-07-08 12:20:30 +0200

    mqtt: explain topic parts directly in the code comments

--- scott-mqtt/mqtt.js
+++ scott-mqtt/mqtt.js
@@ -3,6 +3,15 @@ const message = require('./message')
 const { crc } = require('./crc')
 
 
+/**
+ * TOPIC COMPOSITION (full topic is head + crchead + crcpayload)
+ * 
+ * 131/100 messages from tecnalia ws
+ * 100/100 whatever region
+ * 110 tecnalia company
+ * 110 messages from harvesters (Idoia can also send 131/100 messages but this value is different than 110, 110 represents MY messages)
+ * 101 status processed (because the messages I send are always valid because validation/discadrding is done upstream in the SCOTTProducer)
+ */
 const TOPIC_HEAD = '131/100/100/100/110/110/101'
 
 const connectOptions = {
@@ -29,9 +38,10 @@ function send(harvesterID) {
     console.log('connected to mqtt server')
     const payload = message(harvesterID)
     const cleanPayload = payload.replace(/(\r\n|\n|\r|\t|\s)/gm, '') // INDRA wants the payload to keep small
-    //SERVICE/SUBSERVICE/REGION/SUBREGION/SOURCE/SUBSOURCE/STATUS/CRCHead/CRCPay
+    const CRCHead = crc(cleanHead)
+    const CRCPay = crc(cleanPayload)
     const cleanHead = TOPIC_HEAD.replace('/', '')
-    const topic = `${TOPIC_HEAD}/${crc(cleanHead)}/${crc(cleanPayload)}`
+    const topic = `${TOPIC_HEAD}/${CRCHead}/${CRCPay}` // SERVICE/SUBSERVICE/REGION/SUBREGION/SOURCE/SUBSOURCE/STATUS/CRCHead/CRCPay
     client.publish(topic, cleanPayload, { qos: 1 }, msg => { console.log("Response: " + msg); client.end() }, err => console.error("Error: " + err))
   })
 }
