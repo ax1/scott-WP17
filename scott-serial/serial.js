e8ab830363eab2345ab3043c8122f10addb704db
Angel Rego <angelrego@gmail.com>
2019-09-30 19:03:30 +0200

    serial: better handling on stream errors in the serial port

--- scott-serial/serial.js
+++ scott-serial/serial.js
@@ -3,11 +3,15 @@ const Readline = require('@serialport/parser-readline')
 
 function init(cb) {
   const port = new SerialPort('/dev/ttyUSB0', { baudRate: 9600 })
-  port.on('error', err => { console.error(err); process.exit(1) })
-  port.on('close', err => { console.error(err); process.exit(1) })  // if temporary disconnection, error event is not triggered, but close is, so treat close the same as error
+  port.on('error', stopGraceFully)
+  port.on('close', stopGraceFully)  // if temporary disconnection, error event is not triggered, but close is, so treat close the same as error
   const parser = new Readline()
   port.pipe(parser)
   parser.on('data', line => cb(line.replace(" ", "").replace('\r', '')))
+  parser.on('error', stopGraceFully)
 }
 
+function stopGraceFully(err) {
+  console.error(err); process.exit(1)
+}
 module.exports = { init }
